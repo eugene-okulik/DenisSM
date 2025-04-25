75b353fdefdb1720eaf11d231e5be0dbdccaf68d
Денис Смирнов <arb.smirnov@gmail.com>
2025-04-25 21:14:37 +0300

    update_commit

---
+++ homework/Denis_Smirnov/homework_3/task_1.py
@@ -0,0 +1,11 @@
+#Даны 2 числа a и b. Получить их сумму, разность и произведение
+a = 5
+b = 3
+
+ab_summ = a + b
+ab_subtraction = a - b
+ab_multiplication = a * b
+
+print(f"Сумма {a} + {b} = {ab_summ}")
+print(f"Разность {a} - {b} = {ab_subtraction}")
+print(f"Произведение {a} * {b} = {ab_multiplication}")
---
+++ homework/Denis_Smirnov/homework_3/task_3.py
@@ -0,0 +1,9 @@
+#Даны два числа. Найти среднее арифметическое и среднее геометрическое этих чисел
+a = 5
+b = 3
+
+ab_arithmetic_mean = (a + b) / 2
+ab_geometric_mean = (a * b)**0.5
+
+print(f"Среднее арифметическое для {a} и {b} = {ab_arithmetic_mean}")
+print(f"Среднее геометрическое для {a} и {b} = {ab_geometric_mean}")
